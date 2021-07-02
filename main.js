baa8eefac60d952af579f3d39f937888f3ecd464
123JackCole <123jack.cole@gmail.com>
2021-07-02 07:40:36 -0700

    updating default code

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+const roleHarvester = require('role.harvester');
+const roleUpgrader = require('role.upgrader');
+const roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
-    var tower = Game.getObjectById('8bdf35ef8ef5eae003b354fb');
+    const tower = Game.getObjectById('8bdf35ef8ef5eae003b354fb');
     if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+        const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
         });
         if(closestDamagedStructure) {
             tower.repair(closestDamagedStructure);
         }
 
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if(closestHostile) {
             tower.attack(closestHostile);
         }
     }
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
+    for(const name in Game.creeps) {
+        const creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
